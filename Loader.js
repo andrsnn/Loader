a2fadfbd6f33cb6244721f138fe42b68693834f5
aanderson <aidananderson@projectpals.com>
2015-07-09 22:34:54 -0700

    bower

--- Loader.js
+++ Loader.js
@@ -1,12 +1,30 @@
 define(function(require){
+	
 	var Loader = function(){
 		this.dependencies = {};
-		Loader.prototype.require = function(name){
+		Loader.prototype.require = function(arr, callback){
 			var self = this;
-			require([name], function(ret){
-				self.dependencies[]
-				debugger;
-			});
+
+
+			
+			/*
+			(function (){
+				
+				if (typeof self.dependencies[name] !== "undefined"){
+					
+					return;
+				}
+				require([path], function(ret){
+				//console.log(ret.constructor.name);
+				self.dependencies[name] = ret;
+				
+				
+				//self.dependencies[]
+				
+				});
+			})();
+			*/
+			
 		}
 	}
 
