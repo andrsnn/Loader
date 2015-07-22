b796ba789e1e99bbaff27a0f7e123dadd2011d7f
andrsnn <aidan.anderson.359@my.csun.edu>
2015-07-21 21:23:24 -0700

    added unit tests and finished require/run, load.go

--- Loader.js
+++ Loader.js
@@ -1,7 +1,19 @@
 define(['jquery', 'require','underscore'],function($, require, _){
+	//static mapping of modules to filenames
+	var staticMap = {};
 
-	var AsyncLoader = function(type) {
-	    this.type = type;
+	var AsyncLoader = function(options) {
+		var dependencyName = "dependencies.json";
+		var dependencyProperty = null;
+
+		if (typeof options !== "undefined"){
+			dependencyName = options.dependencyName || dependencyName;
+	    	//below might not work
+	    	
+	    	dependencyProperty = options.dependencyProperty || dependencyProperty;	
+
+		}
+	    
 
 	    var requireFileNames = [];
 	    var requireDfdList = [];
@@ -9,6 +21,8 @@ define(['jquery', 'require','underscore'],function($, require, _){
 
 	    var fileNamesToLoad = [];
 	    var uniqueDeps = {};
+
+	    var currDfd = null;
 
 	    var fileNameModel = function(fileName){
 	    	this.fileName = fileName;
@@ -43,7 +57,7 @@ define(['jquery', 'require','underscore'],function($, require, _){
 	    		
 	    		for (var i = 0; i < all.length; i++) {
 	    			//if there is a sub promise
-	    			debugger;
+	    			
 	    			if (typeof all[i].then !== "undefined" &&
 						typeof all[i].then === "function"){
 	    				//all[i] = requireFileNames[i] = requireDfdList[i] = nestedPromise index
@@ -51,9 +65,12 @@ define(['jquery', 'require','underscore'],function($, require, _){
 	    				
 	    			}
 	    		}
+
+
 	    		if (nestedPromises.length != 0){
 	    			
 	    			(function(){
+
 	    				$.when.apply($,
 	    				nestedPromises.map(function(elem){
 	    					return elem.promise;
@@ -73,27 +90,34 @@ define(['jquery', 'require','underscore'],function($, require, _){
 				    			last.apply(last,all);
 				    		}
 		    				else {
-		    					dfd.resolve(exp);
+		    					var obj = exp.apply(exp,all);
+	    						dfd.resolve(obj);
 		    				}
 		    			});
 	    			})();
 	    			
 	    		}
-
-
-	    		for (var i = 0; i < requireDfdList.length; i++) {
-	    			debugger;
+	    		else {
+	    			for (var i = 0; i < requireDfdList.length; i++) {
+	    			
 	    			var obj = requireDfdList[i].resolve(all[i]);
 	    			obj = all[i];
+		    		}
+		    		if (typeof exp === "string"){
+		    			var last = all.splice(all.length-1,1);
+		    			dfd.resolve(last);
+		    			last.apply(last,all);
+		    		}
+		    		else {
+		    			var obj = exp.apply(exp,all);
+		    			dfd.resolve(obj);
+		    			
+		    			
+		    		}
 	    		}
-	    		if (typeof exp === "string"){
-	    			var last = all.splice(all.length-1,1);
-	    			dfd.resolve(last);
-	    			last.apply(last,all);
-	    		}
-	    		else {
-	    			dfd.resolve(exp);	
-	    		}
+
+
+	    		
 	    		
 	    	});
 	    	return dfd.promise();
@@ -102,8 +126,9 @@ define(['jquery', 'require','underscore'],function($, require, _){
 	    AsyncLoader.prototype.run = function(func){
 
 	    	var dfd = $.Deferred();
-	    	debugger;
+	    	
 	    	require(requireFileNames, function(){
+
 	    		var all = Array.prototype.slice.call(arguments);
 
 	    		var nestedPromises = [];
@@ -111,7 +136,7 @@ define(['jquery', 'require','underscore'],function($, require, _){
 	    		
 	    		for (var i = 0; i < all.length; i++) {
 	    			//if there is a sub promise
-	    			debugger;
+	    			
 	    			if (typeof all[i].then !== "undefined" &&
 						typeof all[i].then === "function"){
 	    				//all[i] = requireFileNames[i] = requireDfdList[i] = nestedPromise index
@@ -119,6 +144,7 @@ define(['jquery', 'require','underscore'],function($, require, _){
 	    				
 	    			}
 	    		}
+
 
 	    		if (nestedPromises.length != 0){
 	    			
@@ -179,20 +205,132 @@ define(['jquery', 'require','underscore'],function($, require, _){
 
 	    //accepts array of 
 	    AsyncLoader.prototype.with = function(){
-	    	var args = Array.prototype.slice.call(arguments);
 
-	    	var argsIndex = 0;
-	    	for (var i = fileNamesToLoad.length - args.length; i < args.length; i++) {
+	    	function pushArgs(args){
 	    		
-	    		fileNamesToLoad[i].deps = args[argsIndex];
+	    		var argsIndex = 0;
 
-	    		//push each dependency onto unquie object (remove duplicates)
-	    		for (var j = 0; j < args[argsIndex].length; j++) {
-	    			uniqueDeps[args[argsIndex][j]] = args[argsIndex][j];
+		    	for (var i = fileNamesToLoad.length - args.length; i < args.length; i++) {
+		    		
+		    		fileNamesToLoad[i].deps = args[argsIndex];
+
+		    		//push each dependency onto unquie object (remove duplicates)
+		    		for (var j = 0; j < args[argsIndex].length; j++) {
+		    			uniqueDeps[args[argsIndex][j]] = args[argsIndex][j];
+		    			
+		    		}
+		    		argsIndex++;
+
+		    	}	
+	    	}
+
+	    	var args = Array.prototype.slice.call(arguments);
+	    	
+	    	if (args.length == 0){
+	    		//!!!! ONLY WORKS FOR A SINGLE MODULE IN FILENAMESTOLOAD
+	    		currDfd = $.Deferred();
+	    		ori = currDfd;
+	    		currDfd = currDfd.promise();
+
+	    		//default to current path + dependencies.json
+
+	    		var jsonReqs = [];
+	    		for (var i = 0; i < fileNamesToLoad.length; i++) {
+	    			var file = fileNamesToLoad[i];
+	    			
+	    			var path = file.fileName.substring(0,file.fileName.lastIndexOf('/')+1);
+	    			path += dependencyName;
+	    			
+	    			
+	    			jsonReqs.push($.getJSON(path));
+	    			
 	    			
 	    		}
+	    		
+	    		$.when.apply($,jsonReqs)
+	    			.done(function(data){
+	    				var a = Array.prototype.slice.call(arguments);
+	    				args = [];
+	    				//check for sub array
+	    				var temp = [];
+	    				if (Object.prototype.toString.call(a[0]) === '[object Array]'){
+	    					
+	    					for (var i = 0; i < a.length; i++) {
+	    						
+	    						
+	    						var toPush = a[i][0];
+	    						temp.push(toPush);
+	    					}
+	    					data = temp;
+	    				}
+	    				else {
+	    					var temp = [];
+	    					temp.push(data);
+	    					data = temp;
+	    				}
+	    				
+	    				
+	    				
 
+	    				for (var i = 0; i < data.length; i++) {
+	    					args.push([]);
+	    					if (dependencyProperty != null){
+
+		    					data[i] = data[i][dependencyProperty];
+		    					if (typeof data === "undefined"){
+		    						throw new Error("dependencyProperty argument incorrect, returning undefined on JSON object with sucessful Ajax");
+		    					}
+		    				}
+	    					Object.keys(data[i]).forEach(function(key,value){
+	    					
+	    						args[i].push(data[i][key]);
+	    					});
+	    				}
+
+	    				
+
+	    				pushArgs(args);
+	    				
+	    				ori.resolve();
+	    			})
+	    			.fail(function(err){
+	    				console.error("Your JSON file is likely bad, must use double quote for strings, or bad path.");
+	    				console.error(err);
+
+	    				
+	    				ori.resolve();
+	    			});	
+	    		
+	    		
 	    	}
+	    	else if (args.length > 0 && typeof args[0] === "string"){
+	    		currDfd = $.Deferred();
+	    		ori = currDfd;
+	    		currDfd = currDfd.promise();
+	    		var req = $.getJSON(args[0])
+	    			.done(function(data){
+	    				args = [[]];
+	    				Object.keys(data).forEach(function(key,value){
+	    					
+	    					args[0].push(data[key]);
+	    				});
+	    				pushArgs(args);
+	    				
+	    				ori.resolve();
+	    			})
+	    			.fail(function(err){
+	    				console.error("Your JSON file is likely bad, must use double quote for strings, or bad path.");
+	    				console.error(err);
+
+	    				
+	    				ori.resolve();
+	    			});
+	    	}
+	    	else {
+	    		pushArgs(args);
+	    	}
+
+	    	
 
 
 
@@ -202,163 +340,87 @@ define(['jquery', 'require','underscore'],function($, require, _){
 	    AsyncLoader.prototype.go = function(cb){
 	    	var dfd = $.Deferred();
 
-	    	//fetch all dependencies and modules
+	    	function goMain(){
+		    		//fetch all dependencies and modules
 
-	    	var commonDeps = Object.keys(uniqueDeps).map(function(key){
-	    		
-	    		return uniqueDeps[key];
-	    	});
+		    	var commonDeps = Object.keys(uniqueDeps).map(function(key){
+		    		
+		    		return uniqueDeps[key];
+		    	});
 
-	    	var fileNames = fileNamesToLoad.map(function(elem){
-	    		return elem.fileName;
-	    	});
+		    	var fileNames = fileNamesToLoad.map(function(elem){
+		    		return elem.fileName;
+		    	});
 
-	    	var all = commonDeps.concat(fileNames);
+		    	var all = commonDeps.concat(fileNames);
 
-	    	
+		    	
 
-	    	//each module should return a factory
-	    	
-    		require(all, function(){
+		    	//each module should return a factory
+		    	
+	    		require(all, function(){
 
-	    		var resolvedModules = Array.prototype.slice.call(arguments);
-	    		var dependencies = resolvedModules.splice(fileNamesToLoad.length-1,resolvedModules.length-1);
+		    		var resolvedModules = Array.prototype.slice.call(arguments);
+		    		
+		    		var dependencies = resolvedModules.splice(0,(resolvedModules.length - fileNamesToLoad.length));
+		    		//fileNamesToLoad.length-1
+		    		
+		    		//dependencies.length should equal length of uniqueDeps
 
-	    		//dependencies.length should equal length of uniqueDeps
+		    		Object.keys(uniqueDeps).forEach(function(elem,index){
+		    			uniqueDeps[elem] = dependencies[index];
+		    			
+		    		});
+		    		
+		    		for (var i = 0; i < fileNamesToLoad.length; i++) {
+		    			for (var j = 0; j < fileNamesToLoad[i].deps.length; j++) {
+		    				
+		    				var currDep = fileNamesToLoad[i].deps;
+		    				currDep[j] = uniqueDeps[currDep[j]];
+		    			}
+		    			
+		    		}
 
-	    		Object.keys(uniqueDeps).forEach(function(elem,index){
-	    			uniqueDeps[elem] = dependencies[index];
-	    			
+		    		
+		    		
+		    		console.assert(resolvedModules.length == fileNamesToLoad.length, "fileNamesToLoad length is not equal to go() async require arguments");
+
+		    		//for each resolved module apply necessary dependencies
+		    		for (var i = 0; i < resolvedModules.length; i++) {
+		    			if (typeof resolvedModules[i] === "undefined" ||
+		    				typeof resolvedModules[i] !== "function"){
+		    				throw new Error("Lazy Loaded module must provide a factory function that will be called by AsyncLoader");
+		    			}
+		    			//store returned object
+
+		    			resolvedModules[i] = resolvedModules[i].apply(null,fileNamesToLoad[i].deps);
+		    		}
+
+		    		
+		    		dfd.resolve.apply(null,resolvedModules);
+		    		cb && cb.apply(cb,resolvedModules);
+
+
+		    	});
+	    	}
+
+	    	if (currDfd != null){
+	    		$.when(currDfd).done(function(){
+	    			goMain();
 	    		});
-	    		
-	    		for (var i = 0; i < fileNamesToLoad.length; i++) {
-	    			for (var j = 0; j < fileNamesToLoad[i].deps.length; j++) {
-	    				
-	    				fileNamesToLoad[i].deps[j] = uniqueDeps[fileNamesToLoad[i].deps[j]];
-	    			}
-	    			
-	    		}
+	    	}
+	    	else {
+	    		goMain();
+	    	}
 
-	    		
-	    		
-	    		console.assert(resolvedModules.length == fileNamesToLoad.length, "fileNamesToLoad length is not equal to go() async require arguments");
-
-	    		//for each resolved module apply necessary dependencies
-	    		for (var i = 0; i < resolvedModules.length; i++) {
-	    			if (typeof resolvedModules[i] === "undefined" ||
-	    				typeof resolvedModules[i] !== "function"){
-	    				throw new Error("Lazy Loaded module must provide a factory function that will be called by AsyncLoader");
-	    			}
-	    			//store returned object
-	    			resolvedModules[i] = resolvedModules[i].apply(null,fileNamesToLoad[i].deps);
-	    		}
-
-	    		
-	    		dfd.resolve.apply(null,resolvedModules);
-	    		cb && cb.apply(cb,resolvedModules);
-
-
-	    	});
+	    	
 	    	
 	    	
 
 	    	return dfd.promise();
 	    }
 
-	    //only handles 1 level of tested promises
-	    //mmake recursive?
-	    AsyncLoader.prototype.get = function(){
-	    	
-	    	var fileNames = Array.prototype.slice.call(arguments);
-		        var dfd = $.Deferred();
-		        var path;
-		        if (typeof this.type === "undefined"){
-		        	path = "";
-		        }
-		        else {
-		        	path = this.type;
-		        	//path = this.type + "/";
-		        }
-		        
-		 
-		        fileNames = _.map(fileNames, function(fileName){
-		            return path + fileName;
-		        });
-		 	
-		        require(fileNames, function() {
-		        	
-		        	
-		        	var x = Array.prototype.slice.call(arguments);
-		        	var allNestedPromises = [];
-		        	debugger;
-		        	x.forEach(function(elem,index){
-		        		var nestedBool = 
-		        			(typeof elem === "object" &&
-		        			typeof elem.nestedPromise !== "undefined" &&
-		        			elem.nestedPromise == true)
-		        			 ? true : false;
-
-		        		//if there is a nested promise do a DFS of the object
-		        		if (nestedBool){
-		        			(function DFS(obj){
-
-		        				for (var key in obj){
-		        					if (obj.hasOwnProperty(key)){
-		        						//if property is a promise
-		        						if (
-		        							typeof obj[key].then !== "undefined" &&
-		        							typeof obj[key].then === "function"
-		        						){
-		        							allNestedPromises.push(obj[key]);
-		        						}
-		        						else if(typeof obj[key] === "object"){
-		        							//recurse
-		        							DFS(obj[key]);
-		        						}
-		        					}
-		        				}
-		        				return;
-		        			})(elem);
-		        			$.when(allNestedPromises,function(result){
-
-		        			});
-		        		}
-
-		        	});
-		        	/*
-		        	var nestedPromise = [];
-		        	for (var i = 0; i < x.length; i++) {
-
-		        		if (typeof x[i] !== "undefined" &&
-		        			typeof x[i].then === "function"){
-		        			
-		        			nestedPromise.push(x[i]);
-		        		}
-		        	}
-		        	if (nestedPromise.length > 0){
-		        		
-		        		$.when.apply(this, nestedPromise).done(function(){
-		        			
-		        			var y = Array.prototype.slice.call(arguments);
-		        			for (var i = 0; i < y.length; i++) {
-		        				x[x.indexOf(nestedPromise[i])] = y[i];
-		        				
-		        			}
-		        			
-							
-		        			dfd.resolve.apply(dfd, x.concat(y));
-		        		});
-		        	}
-		        	else {
-		        		
-		        		dfd.resolve.apply(dfd, arguments);	
-		        	}
-		        	
-		            */
-		        });
-		 		
-		        return dfd.promise();
-	    }
+	    
 	}
 	return AsyncLoader;
 });
